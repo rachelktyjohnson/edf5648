46d87aa8e24e84b6ee8b5e678c85c815058d2af0
Rachel Johnson <rachelktyjohnson@gmail.com>
2023-10-15 14:23:53 +1100

    quality of life additions

--- index.js
+++ index.js
@@ -76,4 +76,22 @@ headings.forEach((heading)=>{
 
 outline.innerHTML = outline_str
 
+const top_button = document.getElementById("top-button");
+top_button.addEventListener('click', topFunction)
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        top_button.style.display = "block";
+    } else {
+        top_button.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
